4a0ca0d1b7662ab59228b929b472150721ec364c
horhik <horhik@disroot.org>
2024-08-31 15:04:16 +0300

    refactor througput computing

--- throughput-compute.py
+++ throughput-compute.py
@@ -7,45 +7,30 @@ import pandas as pd
 
 # Reading DownLink RLS Stats
 
-dl = pd.read_csv('DlRlcStats.txt', sep='\s+')
-for i in range(len(dl.columns) -1, 0, -1):
-    last = dl.columns[i]
-    prev = dl.columns[i - 1]
-    dl[last] = dl[prev]
-dl = dl.drop(columns="%")
+def readRLCStats(filename):
+    df = pd.read_csv(filename, sep='\s+')
+    for i in range(len(df.columns) -1, 0, -1):
+        last = df.columns[i]
+        prev = df.columns[i - 1]
+        df[last] = df[prev]
 
+    df = df.drop(columns="%")
+    return df
 
-# Reading UP Link RLS Stats
+def compute_throughput(df):
+    df['TimeInterval'] = df['end'] - df['start']
+    df['Throughput'] = (df['TxBytes'] * 8) / (df['TimeInterval'] * 1e6)  # Mbps
 
+    throughput_per_user = df.groupby('IMSI')['Throughput'].mean().reset_index()
+    return throughput_per_user
 
-ul = pd.read_csv('DlRlcStats.txt', sep='\s+')
-for i in range(len(ul.columns) -1, 0, -1):
-    last = ul.columns[i]
-    prev = ul.columns[i - 1]
-    ul[last] = ul[prev]
-ul = ul.drop(columns="%")
+dl = readRLCStats("DlRlcStats.txt")
+ul = readRLCStats("UlRlcStats.txt")
 
-
-
-# Computing Throughput for Down Link
-dl['TimeInterval'] = dl['end'] - dl['start']
-dl['Throughput_DL'] = (dl['TxBytes'] * 8) / (dl['TimeInterval'] * 1e6)  # Mbps
-
-# Computing Throughput for UP Link
-ul['TimeInterval'] = ul['end'] - ul['start']
-ul['Throughput_UL'] = (ul['TxBytes'] * 8) / (ul['TimeInterval'] * 1e6)  # Mbps
-
-
-
-# Grouping by IMSI and computing mean
-dl_throughput_per_user = dl.groupby('IMSI')['Throughput_DL'].mean().reset_index()
-ul_throughput_per_user = ul.groupby('IMSI')['Throughput_UL'].mean().reset_index()
-
-
-# Printing results
 print("\n\nFinal result! \n\n")
-print("DL Throughput per user (Mbps):")
-print(dl_throughput_per_user)
 
-print("\nUL Throughput per user (Mbps):")
-print(ul_throughput_per_user)
+print("DL Throughput per user (Mbps):")
+print(compute_throughput(dl), "\n")
+
+print("UL Throughput per user (Mbps):")
+print(compute_throughput(ul), "\n")
